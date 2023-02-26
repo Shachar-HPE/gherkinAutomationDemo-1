39c423cf97c773808ec6d846f4c816a58f0f3112
ohadsam <ohadsam@gmail.com>
2023-02-26 15:48:19 +0200

    Update MyTest.java

--- src/test/java/pack/MyTest.java
+++ src/test/java/pack/MyTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\MyTest_OctaneGherkinResults.xml",
+@CucumberOptions(plugin="com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\MyTest_OctaneGherkinResults1.xml",
                 features="src/test/resources")
 public class MyTest {
 }
